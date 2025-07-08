b53df79ed1120cc675c16723dcaca4a76b28ffdb
clement-lnr <clementlanneretonne@yahoo.fr>
2025-07-08 10:00:17 +0200

    feat: getProductsWithFilters

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,8 +2,9 @@ const productControllers = require("../controllers/products.controllers");
 
 async function productsRoutes(fastify, options) {
     fastify.post("/createProduct", productControllers.createProduct);
-    fastify.get("/getAllProducts", productControllers.getAllProducts);
+    fastify.post("/getAllProducts", productControllers.getAllProducts);
     fastify.get("/getProductById/:id", productControllers.getProductById);
-}
+    fastify.post("/getProductsWithFilters", productControllers.getProductsWithFilters);
+} 
 
 module.exports = productsRoutes;
